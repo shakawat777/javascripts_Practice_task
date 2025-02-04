7d5abaad87c0f416755a774a28d6b2536352349b
shakawat777 <hossainshakawat25@gmail.com>
2025-02-05 00:55:56 +0600

    array combaining

---
+++ Array/combaining_array.js
@@ -0,0 +1,6 @@
+const fruits = ['Apple','Orange','Banana','Mango','Watermelon'];
+const vegetables = ['Tomato','Pumpkin','Cucumber','Carrot','Beetroot'];
+combinedArray = fruits.concat(vegetables);
+console.log(fruits);
+console.log(vegetables);
+console.log(combinedArray);
