216c3274b4306f615512ec77874ad370311c60d4
shakawat777 <hossainshakawat25@gmail.com>
2025-02-04 21:22:01 +0600

    second task done

---
+++ Array/Array_task-2.js
@@ -0,0 +1,9 @@
+const touristSport = ['Hajek','cox-Bazar','Rangamati'];
+touristSport.push('Khagrasore');
+console.log(touristSport);
+touristSport.push('Bandorban','sentmartin');
+console.log(touristSport);
+
+remove_touristSport = touristSport.pop();
+console.log(remove_touristSport);
+console.log(touristSport);
