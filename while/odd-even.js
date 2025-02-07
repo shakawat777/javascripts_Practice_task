ce99a3d92b78a2df1b3d215ea9664cdf84a59f6c
shakawat777 <hossainshakawat25@gmail.com>
2025-02-07 13:02:28 +0600

    find odd and even number

---
+++ while/odd-even.js
@@ -0,0 +1,19 @@
+// find odd number:
+// let num = 61;
+// while(num<= 100){
+//     if(num % 2!= 0){
+//         console.log(num);
+
+//     }
+//     num++; // increment the number by 1
+// }
+
+// Find Even number:
+num =  78;
+while(num <= 98){
+    if(num % 2 == 0){
+
+        console.log(num);
+    }
+    num++; // increment the number by 1
+}
