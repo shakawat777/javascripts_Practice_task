fdd566557eb19ecea2ad44b828799c2643df0912
shakawat777 <hossainshakawat25@gmail.com>
2025-02-08 00:32:58 +0600

    total sum of odd and even numbers

---
+++ while/sum-of-odd-even.js
@@ -0,0 +1,29 @@
+// Total sum of Odd numbers:
+
+
+
+// let sum =0;
+// let number = 81;
+// while(number <= 131){
+//     if(number % 2!= 0){
+//         sum += number;
+//     }
+//     number++;
+    
+// }
+// console.log("sum of Odd numbers:", sum);
+
+
+// Total sum of Even numbers:'
+
+let sum = 0;
+let number = 206;
+while(number <= 311){
+    if(number % 2 == 0){
+        sum += number;
+       
+    }
+    number++;
+
+}
+console.log(" Total sum of Even numbers:",sum);
