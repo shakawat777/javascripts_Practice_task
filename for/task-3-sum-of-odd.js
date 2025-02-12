4f9dc6a2c449042e86b4ae3a7165b9420f3e5410
shakawat777 <hossainshakawat25@gmail.com>
2025-02-12 12:51:41 +0600

    Total sum of odd numbers

---
+++ for/task-3-sum-of-odd.js
@@ -0,0 +1,10 @@
+// Total sum of Odd numbers 91 to 129.
+let number = 129;
+let sum = 0;
+for (let i = 91; i <= 129; i++){
+    if (i % 2!== 0){
+        sum += i;
+    }
+    // console.log("Total sum of Odd numbers 91 to 129:",sum);
+}
+console.log("Total sum of Odd numbers 91 to 129:",sum);
