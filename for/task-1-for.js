58aef880ad1111db0ee32221b080bb994aecc1c4
shakawat777 <hossainshakawat25@gmail.com>
2025-02-12 12:44:13 +0600

    task 2 complete with for loop

---
+++ for/task-1-for.js
@@ -0,0 +1,4 @@
+let n= 60;
+for (let i =0 ; i < n ; i++){
+    console.log("I will invest at list 6 hours every single day last 60 days",i);
+}
---
+++ for/task-2-odd-even.js
@@ -0,0 +1,16 @@
+//ODD Number list from 61 to 100.
+// let number = 100;
+// for (let i = 61 ; i <=number; i++){
+//     if (i % 2!== 0){
+//         console.log(i);
+//     }
+
+// }
+
+//Even number list from 78 to 98.
+let number = 98;
+for( let i = 78 ; i <= number; i++){
+    if( i % 2 == 0){
+        console.log(i);
+    }
+} 
