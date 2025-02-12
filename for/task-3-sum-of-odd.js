e7cd0af996d910a00f27139ee470d3b4fa7f1c61
shakawat777 <hossainshakawat25@gmail.com>
2025-02-12 12:56:13 +0600

    Total sum of even numbers

--- for/task-3-sum-of-odd.js
+++ for/task-3-sum-of-odd.js
@@ -1,10 +1,22 @@
 // Total sum of Odd numbers 91 to 129.
 let number = 129;
+// let sum = 0;
+// for (let i = 91; i <= 129; i++){
+//     if (i % 2!== 0){
+//         sum += i;
+//     }
+//     // console.log("Total sum of Odd numbers 91 to 129:",sum);
+// }
+// console.log("Total sum of Odd numbers 91 to 129:",sum);
+
+// Total sum of Even numbers 51 to 85.
+
+let numbers = 85;
 let sum = 0;
-for (let i = 91; i <= 129; i++){
-    if (i % 2!== 0){
+for (let i = 51; i <= 85; i++){
+    if (i % 2 ===0){
         sum += i;
+         console.log("Total sum of Even numbers 51 to 85:",sum);
     }
-    // console.log("Total sum of Odd numbers 91 to 129:",sum);
 }
-console.log("Total sum of Odd numbers 91 to 129:",sum);
+//console.log("Total sum of Even numbers 51 to 85:",sum);
