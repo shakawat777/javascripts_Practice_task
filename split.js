367eb08932791c672f2e8185e5023198f58ce644
shakawat777 <hossainshakawat25@gmail.com>
2025-08-11 17:58:52 +0600

    add with another function

--- split.js
+++ split.js
@@ -1,3 +1,4 @@
 const sentence = "JavaScript is a bad Programming language.";
-const devided = sentence.split(" ",3);
+//const devided = sentence.split(" ",3);
+const devided = sentence.split("a");
 console.log(devided);
