33e5cf6fa87ee562b144cc515b1a7d489425ba53
shakawat777 <hossainshakawat25@gmail.com>
2025-08-11 17:35:21 +0600

    with function

--- s.js
+++ s.js
@@ -1,3 +1,15 @@
+/*
 const num = "JavaScriprs is a bad programming language";
-const word = num.slice(2,6);
+const word = num.slice(1,3);
 console.log(word);
+*/
+//console.log(word);
+
+//Function: slice array prototype.
+/*
+function example() {
+  let args = Array.prototype.slice.call(arguments);
+  console.log(args); // turns arguments into a real array
+}
+example(1, 2, 3);
+*/
