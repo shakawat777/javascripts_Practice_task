1347bab39b73690015fc2a96a794fd2c80de9f40
shakawat777 <hossainshakawat25@gmail.com>
2025-08-12 17:43:16 +0600

    add split another function

--- split.js
+++ split.js
@@ -1,4 +1,15 @@
+/*
 const sentence = "JavaScript is a bad Programming language.";
 //const devided = sentence.split(" ",3);
-const devided = sentence.split("a");
+
+const devided = sentence.split(" "); 
+
 console.log(devided);
+*/
+// JavaScript string start With() case sensative:
+const sentence = "JavaScript is a bad Programming language.";
+// const text= sentence.startsWith("JavaScript"); // True.
+// const text= sentence.startsWith("JavaScript",1); // False.
+const text= sentence.startsWith("Javascript"); // False.
+// console.log(text);
+console.log(text);
